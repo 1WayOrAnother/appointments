33822e93c2d12f33cd0e6cf560d75028074443ea
Paul Kotok and Zach Evans <zach@>
2017-11-29 13:30:31 -0800

    Add alert of appointment details.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
 	$("#formOne").submit(function(event) {
+	var nameInput = $("input#name").val();
+	var descriptionInput = $("input#description").val();
+  var dateInput = $("input#date").val();
+  var startInput = $("input#start").val();
+  var endInput = $("input#end").val();
+  var info = "Your appointment details: " + nameInput + ", " + descriptionInput + ", " + dateInput + ", " + startInput + ", " + endInput;
+
+  $(".name").text(nameInput);
+  $(".description").text(descriptionInput);
+  $(".date").text(dateInput);
+  $(".start").text(startInput);
+  $(".end").text(endInput);
+
   event.preventDefault();
-	var nameInput = $("input#add1").val();
-	var descriptionInput = $("input#add2").val();
-  var dateInput = $("input#add1").val();
-  var startInput = $("input#add2").val();
-  var endInput = $("input#add2").val();
-	$("#addoutput").text(addresult);
+
+  alert(info);
+	//$("#addoutput").text(addresult);
  	});
 });
